af6a29d6f6eeaedc28dd5d4c3c2791a115182c6b
aliasbinman <aliasbinman@gmail.com>
2018-08-03 20:00:15 -0700

    Remove Some old code

--- NoiseGen/GUI.cpp
+++ NoiseGen/GUI.cpp
@@ -4,7 +4,6 @@
 #include "imgui\imgui_impl_dx11.h"
 
 
-int   g_GUI_TriFillMode = TriFillMode_SOLIDWIRE;
 bool show_Details_Window = true;
 bool show_test_window = true;
 
@@ -38,30 +37,10 @@ void ShowDetailsView()
 
 	if (ImGui::CollapsingHeader("Render Options",ImGuiTreeNodeFlags_DefaultOpen))
 	{
-		static float f = 0.0f;
-		ImGui::Text("Render Options");
-		ImGui::RadioButton("Solid",      &g_GUI_TriFillMode, TriFillMode_SOLID);        ImGui::SameLine();
-        ImGui::RadioButton("WireFrame",  &g_GUI_TriFillMode, TriFillMode_WIRE);         ImGui::SameLine();
-        ImGui::RadioButton("Solid+Wire", &g_GUI_TriFillMode, TriFillMode_SOLIDWIRE);
-        
+       
 		ImGui::SliderFloat("LERP", &gGUI.LerpVal, -1.0f, 2.0f, "%1.3f", 1.0f);
 
-		ImGui::InputInt("NoiseOffset", &gGUI.NoiseOffset);
 		ImGui::SliderInt("Iterations", &gGUI.Iterations, 0, 100);
-
-
-//		ImGui::RadioButton("Color",      &g_GUI_TriViewMode, TriViewMode_COLOR);        ImGui::SameLine();
- //       ImGui::RadioButton("Normal",     &g_GUI_TriViewMode, TriViewMode_NORMAL);      
-        
-
-
-//	ImGui::Checkbox("Show Floor", &g_GUI_Show_Floor);
-//	ImGui::Checkbox("Show AABB",  &g_GUI_Show_AABB);
-//	ImGui::Checkbox("Show Model", &g_GUI_Show_Model);
-//	ImGui::Checkbox("Depth Only Rendering", &g_GUI_DepthOnly);
-//
-//	ImGui::SliderFloat("Model Scale", &g_GUI_ModelScale, 0.1f, 10.0f, "", 1.0f);
-//	ImGui::SliderInt("Model Grid",		&g_GUI_ModelGridSz, 1, 5, "");
 	}
 	ImGui::End();
 }
@@ -71,9 +50,6 @@ void ShowDetailsView()
 void ProcessGUI()
 {
 	ImGui_ImplDX11_NewFrame();
-
-//	HandleHotKeys();
-
 
 	// Global Style
 	ImGuiStyle& style = ImGui::GetStyle();
--- NoiseGen/RenderMain.h
+++ NoiseGen/RenderMain.h
@@ -3,15 +3,3 @@
 #pragma once
 
 
-enum ETriFillMode
-{
-	TriFillMode_SOLID		= 0,
-	TriFillMode_WIRE		= 1,
-	TriFillMode_SOLIDWIRE	= 2
-};
-
-enum ETriViewMode
-{
-	TriViewMode_COLOR		= 0,
-	TriViewMode_NORMAL		= 1,
-};
